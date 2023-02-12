e8c87e098ff1daee521b1ad9ce4add2fdfe571f7
Francois Gaudin <118230+fgaudin@users.noreply.github.com>
2023-02-11 18:09:23 -0800

    forecast

--- epaper_station.ino
+++ epaper_station.ino
@@ -5,6 +5,7 @@
 #include <FS.h>
 #include <LittleFS.h>
 #include <ArduinoJson.h>
+#include <TimeLib.h>
 
 struct Settings {
   char ssid[32];
@@ -14,12 +15,13 @@ struct Settings {
 };
 Settings settings;
 
-const char* openWeatherApi = "https://api.openweathermap.org/data/2.5/%s?q=%s&units=metric&APPID=%s";
+const char* openWeatherApi = "https://api.openweathermap.org/data/2.5/%s?q=%s&units=metric&APPID=%s%s";
 const char* weatherEndpoint = "weather";
-const char* forecastEndpoint = "forecast5";
-char weatherUrl[128];
+const char* forecastEndpoint = "forecast";
 
 BearSSL::CertStore certStore;
+
+HTTPClient http;
 
 void loadSettings() {
   File file = LittleFS.open("/settings.json", "r");
@@ -89,6 +91,112 @@ void connectToWifi() {
   Serial.println(WiFi.localIP());
 }
 
+void refreshWeather(BearSSL::WiFiClientSecure *bear) {
+  char url[128];
+  sprintf(url, openWeatherApi, weatherEndpoint, settings.OWLocation, settings.OWApiKey, "");
+  Serial.println(url);
+
+  http.begin(dynamic_cast<WiFiClient&>(*bear), url);
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, http.getString());
+
+  float temp = doc["main"]["temp"];
+  const char *weatherIcon = doc["weather"][0]["icon"];
+  unsigned long sunrise = (int)(doc["sys"]["sunrise"]) + (int)(doc["timezone"]);
+  unsigned long sunset = (int)(doc["sys"]["sunset"]) + (int)(doc["timezone"]);
+
+  char sunrise_t[6];
+  sprintf(sunrise_t, "%02d:%02d", hour(sunrise), minute(sunrise));
+  char sunset_t[6];
+  sprintf(sunset_t, "%02d:%02d", hour(sunset), minute(sunset));
+
+  Serial.print("temp: ");
+  Serial.println(temp);
+  Serial.print("weatherIcon: ");
+  Serial.println(weatherIcon);
+  Serial.print("sunrise: ");
+  Serial.println(sunrise_t);
+  Serial.print("sunset: ");
+  Serial.println(sunset_t);
+
+}
+
+struct forecastDay {
+  char day[4];
+  float morningTemp;
+  char morningWeather[4] = "";
+  float afternoonTemp;
+  char afternoonWeather[4] = "";
+};
+
+float todayAfternoonTemp;
+char todayAfternoonWeather[4] = "";
+
+char * weekdays[7] = {
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+};
+
+void refreshForecast(BearSSL::WiFiClientSecure *bear) {
+  char url[128];
+  sprintf(url, openWeatherApi, forecastEndpoint, settings.OWLocation, settings.OWApiKey, "&cnt=30");
+  Serial.println(url);
+
+  http.begin(dynamic_cast<WiFiClient&>(*bear), url);
+  // http.useHTTP10(true);
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+
+  DynamicJsonDocument doc(16384);  // https://arduinojson.org/v6/assistant/
+  DeserializationError error = deserializeJson(doc, http.getStream());
+  if (error)
+    Serial.println(error.f_str());
+
+  char buff[32];
+  forecastDay forecast[3];
+  int dayIndex = 0;
+
+  unsigned int now = millis() / 1000  + (int)(doc["city"]["timezone"]);
+
+  for (JsonObject list_item : doc["list"].as<JsonArray>()) {
+    unsigned long t = (int)(list_item["dt"]) + (int)(doc["city"]["timezone"]);
+    if ((hour(t) >= 7 && hour(t) <10) || (hour(t) >= 16 && hour(t) <19)) {
+      if(day(t) == day(now)) {
+        todayAfternoonTemp = list_item["main"]["temp"];
+        strcpy(todayAfternoonWeather, list_item["weather"][0]["icon"]);
+      } else {
+        if (strcmp(forecast[dayIndex].morningWeather, "") == 0) {
+          strcpy(forecast[dayIndex].day, weekdays[weekday(t) - 1]);
+          forecast[dayIndex].morningTemp = list_item["main"]["temp"];
+          strcpy(forecast[dayIndex].morningWeather, list_item["weather"][0]["icon"]);
+        } else {
+          forecast[dayIndex].afternoonTemp = list_item["main"]["temp"];
+          strcpy(forecast[dayIndex].afternoonWeather, list_item["weather"][0]["icon"]);
+          dayIndex++;
+        }
+      }
+    }
+  }
+  Serial.println(todayAfternoonTemp);
+  Serial.println(todayAfternoonWeather);
+  for (int i=0; i<3; i++) {
+    Serial.print("forecast for: ");
+    Serial.println(forecast[i].day);
+    Serial.print("m temp: "); Serial.println(forecast[i].morningTemp);
+    Serial.print("m icon: "); Serial.println(forecast[i].morningWeather);
+    Serial.print("a temp: "); Serial.println(forecast[i].afternoonTemp);
+    Serial.print("a icon: "); Serial.println(forecast[i].afternoonWeather);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   
@@ -112,22 +220,8 @@ void setup() {
   // Integrate the cert store with this connection
   bear->setCertStore(&certStore);
 
-  HTTPClient http;
-  Serial.println(weatherUrl);
-  sprintf(weatherUrl, openWeatherApi, weatherEndpoint, settings.OWLocation, settings.OWApiKey);
-  Serial.println(weatherUrl);
-
-  http.begin(dynamic_cast<WiFiClient&>(*bear), weatherUrl);
-  int httpCode = http.GET();
-  Serial.println(httpCode);
-
-  DynamicJsonDocument doc(1024);
-  deserializeJson(doc, http.getString());
-
-  double temp = doc["main"]["temp"];
-  Serial.print("temp: ");
-  Serial.println(temp);
-
+  refreshWeather(bear);
+  refreshForecast(bear);
 }
 
 void loop() {
