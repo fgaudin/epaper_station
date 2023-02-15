788b0371cc80bc26f3545070dca49df09f207a76
Francois Gaudin <118230+fgaudin@users.noreply.github.com>
2023-02-14 20:49:06 -0800

    icon POC

--- epaper_station.ino
+++ epaper_station.ino
@@ -12,6 +12,9 @@
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
 
+#include <FS.h>
+#define FileClass fs::File
+
 typedef struct {
   char ssid[32];
   char password[32];
@@ -22,16 +25,6 @@ typedef struct {
 const char* openWeatherApi = "https://api.openweathermap.org/data/2.5/%s?q=%s&units=metric&APPID=%s%s";
 const char* weatherEndpoint = "weather";
 const char* forecastEndpoint = "forecast";
-
-const char* sun = "Sun";
-const char* mon = "Mon";
-const char* tue = "Tue";
-const char* wed = "Wed";
-const char* thu = "Thu";
-const char* fri = "Fri";
-const char* sat = "Sat";
-
-const char *const weekdays[] = {sun, mon, tue, wed, thu, fri, sat};
 
 struct forecastDay {
   char day[4];
@@ -55,8 +48,16 @@ struct State {
 
 State state;
 
+typedef GxEPD2_3C < GxEPD2_583c_Z83, GxEPD2_583c_Z83::HEIGHT / 4 > Display;
+
+void drawBitmapFromSpiffs(Display display, const char *filename, int16_t x, int16_t y, bool with_color = true);
+
 void setup() {
   Serial.begin(115200);
+  if (!LittleFS.begin()) {
+    Serial.println(F("An Error has occurred while mounting LittleFS"));
+    return;
+  }
 
   byte refresh = B011;  // bus|forecast|weather
   Serial.println("");
@@ -67,6 +68,7 @@ void setup() {
   Serial.println(ESP.getFreeHeap(), DEC);
   refreshDisplay(refresh);
 
+  LittleFS.end();
   Serial.print(F("Setup end: "));
   Serial.println(ESP.getFreeHeap(), DEC);
 }
@@ -102,15 +104,24 @@ void printState() {
   }
 }
 
+void toWeekdayStr(char * dest, int weekday /* 1-indexed */) {
+  const char* sun = "Sun";
+  const char* mon = "Mon";
+  const char* tue = "Tue";
+  const char* wed = "Wed";
+  const char* thu = "Thu";
+  const char* fri = "Fri";
+  const char* sat = "Sat";
+
+  const char *const weekdays[] = {sun, mon, tue, wed, thu, fri, sat};
+  strcpy(dest, weekdays[weekday-1]);
+}
+
 void refreshData(byte refresh) {
   if (refresh <= 0) {
     return;
   }
 
-  if (!LittleFS.begin()) {
-    Serial.println(F("An Error has occurred while mounting LittleFS"));
-    return;
-  }
   Settings settings;
   loadSettings(&settings);
 
@@ -137,10 +148,9 @@ void refreshData(byte refresh) {
   bear = NULL;
 
   disconnectWifi();
-  LittleFS.end();
 }
 
-void displayWeather(GxEPD2_3C < GxEPD2_583c_Z83, GxEPD2_583c_Z83::HEIGHT / 4 > display)
+void displayWeather(Display display)
 {
   display.setRotation(0);
   display.setFont(&FreeMonoBold24pt7b);
@@ -156,11 +166,13 @@ void displayWeather(GxEPD2_3C < GxEPD2_583c_Z83, GxEPD2_583c_Z83::HEIGHT / 4 > d
   Serial.println(now_t);
   char date[11];
   sprintf(date, "%02d/%02d/%02d", month(now_t), day(now_t), year(now_t));
+  char weekdayStr[4] = "";
+  toWeekdayStr(weekdayStr, weekday(now_t));
   do
   {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
-    display.print(weekdays[weekday(now_t) - 1]);
+    display.print(weekdayStr);
     display.setCursor(x + 100, y);
     display.print(date);
     display.setCursor(x, y + 50);
@@ -169,6 +181,10 @@ void displayWeather(GxEPD2_3C < GxEPD2_583c_Z83, GxEPD2_583c_Z83::HEIGHT / 4 > d
     display.print(state.currentWeather);
   }
   while (display.nextPage());
+
+  int16_t w2 = display.width() / 2;
+  int16_t h2 = display.height() / 2;
+  drawBitmapFromSpiffs(display, "01d.bmp", w2, h2, false);
 }
 
 void refreshDisplay(byte refresh) {
@@ -176,7 +192,7 @@ void refreshDisplay(byte refresh) {
     return;
   }
 
-  GxEPD2_3C < GxEPD2_583c_Z83, GxEPD2_583c_Z83::HEIGHT / 4 > display(GxEPD2_583c_Z83(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4)); // GDEW0583Z83 648x480, GD7965
+  Display display(GxEPD2_583c_Z83(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4)); // GDEW0583Z83 648x480, GD7965
   display.init(115200, true, 2, false);
   displayWeather(display);
   display.hibernate();
@@ -314,7 +330,7 @@ void refreshForecast(Settings *settings, BearSSL::WiFiClientSecure *bear) {
         strcpy(state.afternoonWeather, list_item["weather"][0]["icon"]);
       } else {
         if (strcmp(state.forecast[dayIndex].morningWeather, "") == 0) {
-          strcpy(state.forecast[dayIndex].day, weekdays[weekday(t) - 1]);
+          toWeekdayStr(state.forecast[dayIndex].day, weekday(t));
           state.forecast[dayIndex].morningTemp = list_item["main"]["temp"];
           strcpy(state.forecast[dayIndex].morningWeather, list_item["weather"][0]["icon"]);
         } else {
@@ -329,6 +345,223 @@ void refreshForecast(Settings *settings, BearSSL::WiFiClientSecure *bear) {
     }
   }
   Serial.println(F("done"));
+}
+
+static const uint16_t input_buffer_pixels = 800; // may affect performance
+
+static const uint16_t max_row_width = 1872; // for up to 7.8" display 1872x1404
+static const uint16_t max_palette_pixels = 256; // for depth <= 8
+
+uint8_t input_buffer[3 * input_buffer_pixels]; // up to depth 24
+uint8_t output_row_mono_buffer[max_row_width / 8]; // buffer for at least one row of b/w bits
+uint8_t output_row_color_buffer[max_row_width / 8]; // buffer for at least one row of color bits
+uint8_t mono_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 b/w
+uint8_t color_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 c/w
+uint16_t rgb_palette_buffer[max_palette_pixels]; // palette buffer for depth <= 8 for buffered graphics, needed for 7-color display
+
+uint16_t read16(fs::File& f)
+{
+  // BMP data is stored little-endian, same as Arduino.
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(fs::File& f)
+{
+  // BMP data is stored little-endian, same as Arduino.
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
+void drawBitmapFromSpiffs(Display display, const char *filename, int16_t x, int16_t y, bool with_color)
+{
+  fs::File file;
+  bool valid = false; // valid format to be handled
+  bool flip = true; // bitmap is stored bottom-to-top
+  uint32_t startTime = millis();
+  if ((x >= display.epd2.WIDTH) || (y >= display.epd2.HEIGHT)) return;
+  Serial.println();
+  Serial.print("Loading image '");
+  Serial.print(filename);
+  Serial.println('\'');
+#if defined(ESP32)
+  file = SPIFFS.open(String("/") + filename, "r");
+#else
+  file = LittleFS.open(filename, "r");
+#endif
+  if (!file)
+  {
+    Serial.print("File not found");
+    return;
+  }
+  // Parse BMP header
+  if (read16(file) == 0x4D42) // BMP signature
+  {
+    uint32_t fileSize = read32(file);
+    uint32_t creatorBytes = read32(file); (void)creatorBytes; //unused
+    uint32_t imageOffset = read32(file); // Start of image data
+    uint32_t headerSize = read32(file);
+    uint32_t width  = read32(file);
+    int32_t height = (int32_t) read32(file);
+    uint16_t planes = read16(file);
+    uint16_t depth = read16(file); // bits per pixel
+    uint32_t format = read32(file);
+    if ((planes == 1) && ((format == 0) || (format == 3))) // uncompressed is handled, 565 also
+    {
+      Serial.print("File size: "); Serial.println(fileSize);
+      Serial.print("Image Offset: "); Serial.println(imageOffset);
+      Serial.print("Header size: "); Serial.println(headerSize);
+      Serial.print("Bit Depth: "); Serial.println(depth);
+      Serial.print("Image size: ");
+      Serial.print(width);
+      Serial.print('x');
+      Serial.println(height);
+      // BMP rows are padded (if needed) to 4-byte boundary
+      uint32_t rowSize = (width * depth / 8 + 3) & ~3;
+      if (depth < 8) rowSize = ((width * depth + 8 - depth) / 8 + 3) & ~3;
+      if (height < 0)
+      {
+        height = -height;
+        flip = false;
+      }
+      uint16_t w = width;
+      uint16_t h = height;
+      if ((x + w - 1) >= display.epd2.WIDTH)  w = display.epd2.WIDTH  - x;
+      if ((y + h - 1) >= display.epd2.HEIGHT) h = display.epd2.HEIGHT - y;
+      if (w <= max_row_width) // handle with direct drawing
+      {
+        valid = true;
+        uint8_t bitmask = 0xFF;
+        uint8_t bitshift = 8 - depth;
+        uint16_t red, green, blue;
+        bool whitish = false;
+        bool colored = false;
+        if (depth == 1) with_color = false;
+        if (depth <= 8)
+        {
+          if (depth < 8) bitmask >>= depth;
+          //file.seek(54); //palette is always @ 54
+          file.seek(imageOffset - (4 << depth)); // 54 for regular, diff for colorsimportant
+          for (uint16_t pn = 0; pn < (1 << depth); pn++)
+          {
+            blue  = file.read();
+            green = file.read();
+            red   = file.read();
+            file.read();
+            whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+            colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+            if (0 == pn % 8) mono_palette_buffer[pn / 8] = 0;
+            mono_palette_buffer[pn / 8] |= whitish << pn % 8;
+            if (0 == pn % 8) color_palette_buffer[pn / 8] = 0;
+            color_palette_buffer[pn / 8] |= colored << pn % 8;
+          }
+        }
+        uint32_t rowPosition = flip ? imageOffset + (height - h) * rowSize : imageOffset;
+        for (uint16_t row = 0; row < h; row++, rowPosition += rowSize) // for each line
+        {
+          uint32_t in_remain = rowSize;
+          uint32_t in_idx = 0;
+          uint32_t in_bytes = 0;
+          uint8_t in_byte = 0; // for depth <= 8
+          uint8_t in_bits = 0; // for depth <= 8
+          uint8_t out_byte = 0xFF; // white (for w%8!=0 border)
+          uint8_t out_color_byte = 0xFF; // white (for w%8!=0 border)
+          uint32_t out_idx = 0;
+          file.seek(rowPosition);
+          for (uint16_t col = 0; col < w; col++) // for each pixel
+          {
+            // Time to read more pixel data?
+            if (in_idx >= in_bytes) // ok, exact match for 24bit also (size IS multiple of 3)
+            {
+              in_bytes = file.read(input_buffer, in_remain > sizeof(input_buffer) ? sizeof(input_buffer) : in_remain);
+              in_remain -= in_bytes;
+              in_idx = 0;
+            }
+            switch (depth)
+            {
+              case 24:
+                blue = input_buffer[in_idx++];
+                green = input_buffer[in_idx++];
+                red = input_buffer[in_idx++];
+                whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+                colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+                break;
+              case 16:
+                {
+                  uint8_t lsb = input_buffer[in_idx++];
+                  uint8_t msb = input_buffer[in_idx++];
+                  if (format == 0) // 555
+                  {
+                    blue  = (lsb & 0x1F) << 3;
+                    green = ((msb & 0x03) << 6) | ((lsb & 0xE0) >> 2);
+                    red   = (msb & 0x7C) << 1;
+                  }
+                  else // 565
+                  {
+                    blue  = (lsb & 0x1F) << 3;
+                    green = ((msb & 0x07) << 5) | ((lsb & 0xE0) >> 3);
+                    red   = (msb & 0xF8);
+                  }
+                  whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+                  colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+                }
+                break;
+              case 1:
+              case 4:
+              case 8:
+                {
+                  if (0 == in_bits)
+                  {
+                    in_byte = input_buffer[in_idx++];
+                    in_bits = 8;
+                  }
+                  uint16_t pn = (in_byte >> bitshift) & bitmask;
+                  whitish = mono_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                  colored = color_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                  in_byte <<= depth;
+                  in_bits -= depth;
+                }
+                break;
+            }
+            if (whitish)
+            {
+              // keep white
+            }
+            else if (colored && with_color)
+            {
+              out_color_byte &= ~(0x80 >> col % 8); // colored
+            }
+            else
+            {
+              out_byte &= ~(0x80 >> col % 8); // black
+            }
+            if ((7 == col % 8) || (col == w - 1)) // write that last byte! (for w%8!=0 border)
+            {
+              output_row_color_buffer[out_idx] = out_color_byte;
+              output_row_mono_buffer[out_idx++] = out_byte;
+              out_byte = 0xFF; // white (for w%8!=0 border)
+              out_color_byte = 0xFF; // white (for w%8!=0 border)
+            }
+          } // end pixel
+          uint16_t yrow = y + (flip ? h - row - 1 : row);
+          display.writeImage(output_row_mono_buffer, output_row_color_buffer, x, yrow, w, 1);
+        } // end line
+        Serial.print("loaded in "); Serial.print(millis() - startTime); Serial.println(" ms");
+        display.refresh();
+      }
+    }
+  }
+  file.close();
+  if (!valid)
+  {
+    Serial.println("bitmap format not handled.");
+  }
 }
 
 void loop() {
